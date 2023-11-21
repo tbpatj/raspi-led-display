861b9bd12dde5618420ae894d778c44e5fb9b7db
Timothy Luebke <luebketj@gmail.com>
2023-11-21 15:27:22 -0700

    fixed bugsJ

--- ledStrip.cpp
+++ ledStrip.cpp
@@ -1,32 +1,31 @@
 class LEDStrip {
-    private:
-        int led_count;
-        int led_pin;
-        ws2811_channel_t& channel;
-        ws2811_t ledstring
-    public:
-        LEDStrip(int pin, int count){
-            led_pin = pin;
-            led_count = count;
+private:
+    int led_count;
+    int led_pin;
+    ws2811_channel_t* channel;
+    ws2811_t ledstring;
 
-            ledstring = {};
-            ledstring.freq = WS2811_TARGET_FREQ;
-            ledstring.dmanum = 10; // Choose a DMA channel
+public:
+    LEDStrip(int pin, int count) : led_count(count), led_pin(pin) {
+        ledstring = {};
+        ledstring.freq = WS2811_TARGET_FREQ;
+        ledstring.dmanum = 10; // Choose a DMA channel
 
-            channel = ledstring.channel[0];
-            channel.gpionum = LED_PIN;
-            channel.count = LED_COUNT;
-            channel.invert = 0;
-            channel.brightness = 255;
-            channel.strip_type = WS2811_STRIP_GRB;  // Set the correct color order for WS2812B
-           
+        channel = &ledstring.channel[0];
+        channel->gpionum = led_pin;
+        channel->count = led_count;
+        channel->invert = 0;
+        channel->brightness = 255;
+        channel->strip_type = WS2811_STRIP_GRB; // Set the correct color order for WS2812B
+    }
+
+    bool init() {
+        if (ws2811_init(&ledstring)) {
+            std::cerr << "ws2811_init failed" << std::endl;
+            return false;
         }
-        bool init(){
-             if (ws2811_init(&ledstring)) {
-                std::cerr << "ws2811_init failed" << std::endl;
-                return false;
-            }
-            return true;
-        }
-       
+        return true;
+    }
+
+    // Add any other methods or functionality as needed
 };
