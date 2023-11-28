c11c6886245aae69bd5779b50624dc1bdf409838
Timothy Luebke <luebketj@gmail.com>
2023-11-28 15:02:28 -0700

    feat: testing out the server running with the other details

--- displayLeds.cpp
+++ displayLeds.cpp
@@ -25,73 +25,44 @@ int main(){
         std::cout << "hit" << std::endl;
     });
 
-    // You can add more routes for different paths
-
     svr.listen("0.0.0.0", 3000);
+
+    // You can add more routes for different paths
+    
+    FrameObject fo(100);
+    //make sure to downsample the frame so we are able to blur faster and not working with massive image
+    fo.downsampleFrame();
+    DisplayProperties display(fo.getFrame(),LED_COUNT);
+    display.printValues();
+    display.initLEDPos(LED_POS[0],LED_POS[1],LED_POS[2],LED_POS[3]);
+
+    LEDStrip led(LED_PIN,LED_COUNT);
+    if(!led.init()){
+        return -1;
+    }
+
+    led.setColor(display.rowXPixels,255,0,0);
+    led.setColor(5,0,255,0);
+    led.setColor(10,0,0,255);
+    led.render();
+    while(true){
+        fo.updateFrame();
+        fo.downsampleFrame();
+        fo.blurFrame();
+        
+        // fo.show();
+        if(cv::waitKey(30) >= 0){
+			break;
+	    }
+    }
+    
     std::cout << "test" << std::endl;
     return 0;
 }
 
 // int main(){
 
-//     FrameObject fo(100);
-//     //make sure to downsample the frame so we are able to blur faster and not working with massive image
-//     fo.downsampleFrame();
-//     DisplayProperties display(fo.getFrame(),LED_COUNT);
-//     display.printValues();
-//     display.initLEDPos(LED_POS[0],LED_POS[1],LED_POS[2],LED_POS[3]);
-
-//     LEDStrip led(LED_PIN,LED_COUNT);
-//     if(!led.init()){
-//         return -1;
-//     }
-
-//     led.setColor(display.rowXPixels,255,0,0);
-//     led.setColor(5,0,255,0);
-//     led.setColor(10,0,0,255);
-//     led.render();
-//     while(true){
-//         fo.updateFrame();
-//         fo.downsampleFrame();
-//         fo.blurFrame();
-//         int start = display.clockwise ? display.ledLeftStart : display.ledLeftEnd;
-//         int pos =  display.clockwise ? display.maxY - 1 : 0;
-//         int dir = display.clockwise ? -1 : 1;
-//         cv::Vec3b c;
-//         for(int i = 0; i <= display.leftL; ++i){
-//             int y = pos + display.leftDx * dir * i;
-//             c = fo.getColorAt(0,max(pos + display.leftDx * dir * i,0));
-//             led.setColor(i + start,c[0],c[1],c[2]);
-//         }
-//         start = display.clockwise ? display.ledRightStart : display.ledRightEnd;
-//         pos =  display.clockwise ? 0 : display.maxY - 1;
-//         dir = display.clockwise ? 1 : -1;
-//         for(int i = 0; i <= display.rightL; ++i){
-//             c = fo.getColorAt(display.maxX - 1,max(pos + display.rightDx * dir * i,0));
-//             led.setColor(i + start,c[0],c[1],c[2]);
-//         }
-//         start = display.clockwise ? display.ledBottomStart :display.ledBottomEnd;
-//         pos =  display.clockwise ? display.maxX - 1 : 0;
-//         dir = display.clockwise ? -1 : 1;
-//         for(int i = 0; i <= display.bottomL; ++i){
-//             c = fo.getColorAt(max(pos + display.bottomDx * dir * i,0),display.maxY - 1);
-//             led.setColor(i + start,c[0],c[1],c[2]);
-//         }
-//         start = display.clockwise ? display.ledTopStart :display.ledTopEnd;
-//         pos =  display.clockwise ? 0 : display.maxX - 1;
-//         dir = display.clockwise ? 1 : -1;
-//         for(int i = 0; i <= display.topL; ++i){
-//             c = fo.getColorAt(max(pos + display.rightDx * dir * i,0),0);
-//             led.setColor(i + start,c[0],c[1],c[2]);
-//         }
-//         led.render();
-//         // fo.show();
-//         if(cv::waitKey(30) >= 0){
-// 			break;
-// 	    }
-//     }
     
-// 	return 0;
 
 	
 // }
--- ledStrip.cpp
+++ ledStrip.cpp
@@ -35,5 +35,39 @@ public:
         return true;
     }
 
+    void mapLEDs(FrameObject *fo,DisplayProperties *display) {
+        int start = display.clockwise ? display.ledLeftStart : display.ledLeftEnd;
+        int pos =  display.clockwise ? display.maxY - 1 : 0;
+        int dir = display.clockwise ? -1 : 1;
+        cv::Vec3b c;
+        for(int i = 0; i <= display.leftL; ++i){
+            int y = pos + display.leftDx * dir * i;
+            c = fo.getColorAt(0,max(pos + display.leftDx * dir * i,0));
+            led.setColor(i + start,c[0],c[1],c[2]);
+        }
+        start = display.clockwise ? display.ledRightStart : display.ledRightEnd;
+        pos =  display.clockwise ? 0 : display.maxY - 1;
+        dir = display.clockwise ? 1 : -1;
+        for(int i = 0; i <= display.rightL; ++i){
+            c = fo.getColorAt(display.maxX - 1,max(pos + display.rightDx * dir * i,0));
+            led.setColor(i + start,c[0],c[1],c[2]);
+        }
+        start = display.clockwise ? display.ledBottomStart :display.ledBottomEnd;
+        pos =  display.clockwise ? display.maxX - 1 : 0;
+        dir = display.clockwise ? -1 : 1;
+        for(int i = 0; i <= display.bottomL; ++i){
+            c = fo.getColorAt(max(pos + display.bottomDx * dir * i,0),display.maxY - 1);
+            led.setColor(i + start,c[0],c[1],c[2]);
+        }
+        start = display.clockwise ? display.ledTopStart :display.ledTopEnd;
+        pos =  display.clockwise ? 0 : display.maxX - 1;
+        dir = display.clockwise ? 1 : -1;
+        for(int i = 0; i <= display.topL; ++i){
+            c = fo.getColorAt(max(pos + display.rightDx * dir * i,0),0);
+            led.setColor(i + start,c[0],c[1],c[2]);
+        }
+        led.render();
+    }
+
     // Add any other methods or functionality as needed
 };
